5c710054a7a36aaf949ed037ea7b51f17609632d
Miika Savela <miika.a.savela@gmail.com>
2025-08-11 18:30:30 +0300

    CH1L11: Add basic health check endpoint

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	address := ":8080"
+	log.Printf("Starting server on address %s", address)
 	mux := http.NewServeMux()
+	mux.Handle("/app/",
+		http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handleHealth)
 	server := http.Server{
 		Handler: mux,
-		Addr:    ":8080",
+		Addr:    address,
 	}
 	server.ListenAndServe()
+}
+
+func handleHealth(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(http.StatusText(http.StatusOK)))
 }
