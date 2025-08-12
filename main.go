a3e66f3e044d32cf8d0e31b2cb4a7a214cee458f
Miika Savela <miika.a.savela@gmail.com>
2025-08-12 14:13:03 +0300

    CH2L1: Add middleware to record number of views to app

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareIncrementViews(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits.Load())))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits = atomic.Int32{}
+}
 
 func main() {
 	address := ":8080"
+	cfg := apiConfig{
+		fileServerHits: atomic.Int32{},
+	}
 	log.Printf("Starting server on address %s", address)
 	mux := http.NewServeMux()
 	mux.Handle("/app/",
-		http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", handleHealth)
+		middlewareLogging(
+			cfg.middlewareIncrementViews(
+				http.StripPrefix("/app/", http.FileServer(http.Dir("."))),
+			),
+		),
+	)
+	mux.HandleFunc("GET /healthz", handleHealth)
+	mux.HandleFunc("GET /metrics", cfg.handleMetrics)
+	mux.HandleFunc("POST /reset", cfg.handleReset)
 	server := http.Server{
 		Handler: mux,
 		Addr:    address,
@@ -19,8 +52,15 @@ func main() {
 	server.ListenAndServe()
 }
 
-func handleHealth(writer http.ResponseWriter, req *http.Request) {
-	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte(http.StatusText(http.StatusOK)))
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func middlewareLogging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s request to %s", r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
 }
